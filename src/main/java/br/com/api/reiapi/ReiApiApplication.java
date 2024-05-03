14e378db1619cc14acc8c1f320e3514c48ef9e60
Pedro Carvalho <90916013+sadpreduu@users.noreply.github.com>
2024-05-03 18:00:01 -0300

    initial commit

---
+++ src/main/java/br/com/api/reiapi/ReiApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.reiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/reiapi/ReiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.reiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
